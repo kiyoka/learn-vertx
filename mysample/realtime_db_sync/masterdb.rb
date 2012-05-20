ddbd5c7cb38b49b62f2ba0caed08356f09ade9e8
Kiyoka Nishiyama <kiyoka@sumibi.org>
2012-05-20 15:50:59 +0900

    use memcached for event bus. Problem: notifier can only response for single client.

--- mysample/realtime_db_sync/masterdb.rb
+++ mysample/realtime_db_sync/masterdb.rb
@@ -2,22 +2,25 @@ require 'vertx'
 require 'json/pure'
 require 'pp'
 require 'cgi'
-
-#require 'dbm'
-#masterHash = DBM.new( "/tmp/masterHash" )
+require 'date'
+require 'memcache'
 
 masterHash = {}
+notifyHash = Memcache.new( :server => "localhost:11211" )
 
 masterdb_server = Vertx::HttpServer.new
 masterdb_server.request_handler do |req|
 
-  puts "An HTTP request has been received (method=#{req.method})"
+  puts "MasterDB: An HTTP request has been received"
 
   req.body_handler do |body|
     puts "The total body received was #{body.length} bytes, path is #{req.path}."
 
+    query = CGI::parse( req.query )
+    username = query[ 'username' ].first
+
     params = CGI::parse( body.to_s )
-    values = JSON( params[ 'values' ][0] )
+    values = JSON( params[ 'values' ].first )
 
     case req.path
     when "/putValues"
@@ -29,6 +32,10 @@ masterdb_server.request_handler do |req|
       }
 
       # notify to all client
+      dt = DateTime::now()
+      currentDate = dt.strftime( "%s" ) + " " + dt.strftime( "%x %X" )
+      pp [ "currentDate", currentDate ]
+      notifyHash[ username ] = currentDate
       Vertx::EventBus.send('bus.notify', 'new values came, you must sync.')
 
     when "/getValues"
@@ -39,4 +46,3 @@ masterdb_server.request_handler do |req|
   end
 
 end.listen(8081, 'localhost')
-
--- mysample/realtime_db_sync/notifier.rb
+++ mysample/realtime_db_sync/notifier.rb
@@ -1,38 +1,41 @@
 require 'vertx'
+require 'cgi'
+require 'memcache'
 
 # use http://en.wikipedia.org/wiki/Chunked_transfer_encoding
 
-def notify( res )
-  res.write_str( "notify!\n"  )
-end
+INTERVAL = 0.5
 
-Vertx::EventBus.register_handler('bus.notify') do |message|
-  puts("DUMMY: I received a message from masterdb.rb #{message.body}")  
-end
+notifyHash = Memcache.new( :server => "localhost:11211" )
+#Vertx::SharedData::get_hash("notifyHash")
 
+def notify( res, str )
+  res.write_str( "notify! #{str}\n"  )
+end
 
 notifier = Vertx::HttpServer.new
 notifier.request_handler do |req|
-  puts 'An HTTP request has been received'
+  puts 'Notifier: An HTTP request has been received'
 
   req.end_handler do
-
+    query = CGI::parse( req.query )
+    username = query[ 'username' ].first
     # Now send back a response
     req.response.chunked = true
 
-    handlerId = Vertx::EventBus.register_handler('bus.notify') do |message|
-      puts("I received a message from masterdb.rb #{message.body}")
-      notify( req )
-    end
-
     # wait for notify event.
-    60.times { |i|
-      sleep( 1 )
+    got = nil
+    (10 / INTERVAL).to_i.times { |i|
+      if notifyHash[ username ]
+        if got != notifyHash[ username ]
+          got = notifyHash[ username ]
+          notify( req.response, got )
+        end
+      end
+      sleep( INTERVAL )
     }
 
     req.response.end
-
-    Vertx::EventBus.unregister_handler( handlerId )
   end
 
 end.listen(8080, 'localhost')
